a3041a684751f6e9a450d96913c47a5efc4c7822
Andres Alonzo <aalonzolu@gmail.com>
2025-10-29 09:50:09 -0600

    ASITI 2025

--- js/countdown.js
+++ js/countdown.js
@@ -42,5 +42,5 @@ function initializeClock(id, endtime) {
 }
 
 
-var deadline = new Date("Sep 24, 2023 09:00:00").getTime();
+var deadline = new Date("Nov 16, 2025 09:00:00").getTime();
 initializeClock('clockdiv', deadline);
